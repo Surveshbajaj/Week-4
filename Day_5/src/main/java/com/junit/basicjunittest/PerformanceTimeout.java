7595b55a309b3fde7fcfa29be3f58b40ea83310a
SurveshBajaj <Sarveshbajaj9@gmail.com>
2025-02-15 23:47:17 +0530

    [Survesh Bajaj]- Add. Performance Testing Using @Timeout

---
+++ Day_5/src/main/java/com/junit/basicjunittest/PerformanceTimeout.java
@@ -0,0 +1,10 @@
+package com.junit.basicjunittest;
+
+public class PerformanceTimeout {
+    public static String longRunningTask() throws InterruptedException {
+        Thread.sleep(3000); // Simulating a long task (3 seconds)
+        return "Task Completed";
+    }
+}
+
+
